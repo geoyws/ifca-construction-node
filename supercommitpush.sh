a72b5ad171c70ec3d85d367f940b5b667a3489dc
George Yong Wen Soong <geoyws@gmail.com>
2019-06-24 11:28:00 +0800

    test

--- supercommitpush.sh
+++ supercommitpush.sh
@@ -4,7 +4,7 @@ if [ -z $1 ]; then
     echo "You need to provide a commit message. Try again."
 fi
 
-# this basically commits and pushes everything to save us time
+# commit and push everythinge
 
 . supercommit.sh "$1"
 . superpush.sh
